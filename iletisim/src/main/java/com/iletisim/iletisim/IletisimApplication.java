4a465ec1bbd3cb3d7b3575368df2bd32338f36b8
guderhasan <68234046+guderhasan@users.noreply.github.com>
2025-06-19 14:56:38 +0300

    Add files via upload

---
+++ iletisim/src/main/java/com/iletisim/iletisim/IletisimApplication.java
@@ -0,0 +1,13 @@
+package com.iletisim.iletisim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IletisimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IletisimApplication.class, args);
+	}
+
+}
---
+++ iletisim/src/test/java/com/iletisim/iletisim/IletisimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iletisim.iletisim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IletisimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
